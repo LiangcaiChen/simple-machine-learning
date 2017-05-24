ae237c8daec49872633f56105dd1c12794536fbc
LiangcaiChen <chenl47@cardiff.ac.uk>
2017-05-24 19:18:21 +0100

    add data for multiple linear regression

--- simple-linear-regression/simple_linear_regression.py
+++ simple-linear-regression/simple_linear_regression.py
@@ -43,3 +43,17 @@ plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+# Visualising the test set results
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
+
